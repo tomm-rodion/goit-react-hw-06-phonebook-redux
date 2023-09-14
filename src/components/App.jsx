3c3cf628f5a4404ca31fb3d49968a7f019752925
tomm-rodion <autovolna@gmail.com>
2023-09-14 19:49:40 +0300

    add dispatch addContact and deleleContactById

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,3 @@
-import { nanoid } from 'nanoid';
-
 import { ContactList } from './ContactList/ContactList';
 import { ContactsFilter } from './ContactsFilter/CotactsFilter';
 import { ContactsForm } from './ContactsForm/ContactsForm';
@@ -10,47 +8,57 @@ import {
   TitleContacts,
 } from './App.styled';
 import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addContact, deleteContact } from 'redux/store';
 
-const getInitialContacts = () => {
-  const savedUpContacts = localStorage.getItem('contacts');
-  if (savedUpContacts !== null) {
-    const parsContacts = JSON.parse(savedUpContacts);
-    return parsContacts;
-  }
-  return [
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ];
-};
+// const getInitialContacts = () => {
+//   const savedUpContacts = localStorage.getItem('contacts');
+//   if (savedUpContacts !== null) {
+//     const parsContacts = JSON.parse(savedUpContacts);
+//     return parsContacts;
+//   }
+//   return [
+//     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+//     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+//     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+//     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+//   ];
+// };
 
 export const App = () => {
-  const [contacts, setContacts] = useState(getInitialContacts);
+  const contacts = useSelector(state => state.contacts);
+
+  // const [contacts, setContacts] = useState(getInitialContacts);
   const [filter, setFilter] = useState('');
 
   useEffect(() => {
     localStorage.setItem('contacts', JSON.stringify(contacts));
   }, [contacts]);
 
-  const onformSubmit = ({ name, number }) => {
+  const dispatch = useDispatch();
+
+  const onformSubmit = value => {
     const nameInContacts = contacts.find(
-      contact => contact.name.toLowerCase() === name.toLowerCase()
+      contact => contact.name.toLowerCase() === value.name.toLowerCase()
     );
     //перевірка існуючого кантакта в телефоній книжці.
     if (nameInContacts) {
-      alert(`${name} is already in contacts.`);
+      alert(`${value.name} is already in contacts.`);
       return;
     }
-    // створення нового контакта
-    const newContact = { id: nanoid(), name, number };
-    setContacts(prevContacts => [newContact, ...prevContacts]);
+    // // створення нового контакта
+    const newContact = () => dispatch(addContact(value));
+    newContact();
+    // const newContact = { id: nanoid(), name, number };
+    // setContacts(prevContacts => [newContact, ...prevContacts]);
   };
 
   const onDelete = id => {
-    setContacts(prevContacts =>
-      prevContacts.filter(contact => contact.id !== id)
-    );
+    const deleteontactById = () => dispatch(deleteContact(id));
+    deleteontactById();
+    // setContacts(prevContacts =>
+    //   prevContacts.filter(contact => contact.id !== id)
+    // );
   };
 
   const onFilter = e => {
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,8 @@
 import { createStore } from 'redux';
 import { devToolsEnhancer } from '@redux-devtools/extension';
+import { nanoid } from 'nanoid';
 const initialState = {
-  defaultContact: [
+  contacts: [
     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
@@ -9,8 +10,37 @@ const initialState = {
   ],
 };
 const rootReducer = (state = initialState, action) => {
-  return state;
+  switch (action.type) {
+    case 'contacts/addContact':
+      return {
+        ...state,
+        contacts: [action.payload, ...state.contacts],
+      };
+    case 'contacts/deleteContact':
+      return {
+        ...state,
+        contacts: state.contacts.filter(
+          contact => contact.id !== action.payload
+        ),
+      };
+    default:
+      return state;
+  }
 };
 
 const enhancer = devToolsEnhancer();
 export const store = createStore(rootReducer, enhancer);
+
+export const addContact = ({ name, number }) => {
+  return {
+    type: 'contacts/addContact',
+    payload: { id: nanoid(), name, number },
+  };
+};
+
+export const deleteContact = id => {
+  return {
+    type: 'contacts/deleteContact',
+    payload: id,
+  };
+};
